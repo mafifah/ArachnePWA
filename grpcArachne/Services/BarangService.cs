ecfa7c9503de0d1987eb34e9f0b01c4163e31a6b
Deva Trivanus <devadlsc18@gmail.com>
2021-07-23 08:51:27 +0700

    merge

--- grpcArachne/Services/BarangService.cs
+++ grpcArachne/Services/BarangService.cs
@@ -1,8 +1,7 @@
 using Grpc.Core;
 using grpcArachne.Data;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -18,64 +17,6 @@ namespace grpcArachne
         {
             _logger = logger;
             _db = db;
-        }
-
-        public override Task<ListBarang> GetDataBarang(ListBarangRequest request, ServerCallContext context)
-        {
-            _idSupplier = request.IdSupplier;
-            ListBarang output = new ListBarang();
-            if (_idSupplier == 1001)
-            {
-                var qryBarang =
-                (from T2Barang in _db.T2BarangDbSet
-                 join T3Satuan in _db.T3SatuanDbSet
-                 on T2Barang.IdBarang equals T3Satuan.IdBarang
-                 join T5SupplierSatuan in _db.T5SupplierSatuanDbSet
-                 on T3Satuan.IdSatuan equals T5SupplierSatuan.IdSatuan
-                 join T1Supplier in _db.T1SupplierDbSet
-                 on T5SupplierSatuan.IdSupplier equals T1Supplier.IdSupplier
-                 join T5StokGudang in _db.T5StokGudangDbSet
-                 on T5SupplierSatuan.IdSatuan equals T5StokGudang.IdSatuan
-                 join T5CompanySatuan in _db.T5CompanySatuanDbSet
-                 on T3Satuan.IdSatuan equals T5CompanySatuan.IdSatuan
-                 select new Barang
-                 {
-                     IdBarang = (long)T3Satuan.IdBarang,
-                     Nama = T2Barang.Barang,
-                     Satuan = T3Satuan.Satuan,
-                     Stok = (int)T5StokGudang.StokBaru,
-                     Minimum = T5CompanySatuan.Minimum,
-                     Maksimum = T5CompanySatuan.Maksimum
-                 });
-                output.Barang.AddRange(qryBarang);
-            }
-            else
-            {
-                var qryBarang =
-                (from T2Barang in _db.T2BarangDbSet
-                 join T3Satuan in _db.T3SatuanDbSet
-                 on T2Barang.IdBarang equals T3Satuan.IdBarang
-                 join T5SupplierSatuan in _db.T5SupplierSatuanDbSet
-                 on T3Satuan.IdSatuan equals T5SupplierSatuan.IdSatuan
-                 join T1Supplier in _db.T1SupplierDbSet
-                 on T5SupplierSatuan.IdSupplier equals T1Supplier.IdSupplier
-                 join T5StokGudang in _db.T5StokGudangDbSet
-                 on T5SupplierSatuan.IdSatuan equals T5StokGudang.IdSatuan
-                 join T5CompanySatuan in _db.T5CompanySatuanDbSet
-                 on T3Satuan.IdSatuan equals T5CompanySatuan.IdSatuan
-                 where T5SupplierSatuan.IdSupplier == _idSupplier
-                 select new Barang
-                 {
-                     IdBarang = (long)T3Satuan.IdBarang,
-                     Nama = T2Barang.Barang,
-                     Satuan = T3Satuan.Satuan,
-                     Stok = (int)T5StokGudang.StokBaru,
-                     Minimum = T5CompanySatuan.Minimum,
-                     Maksimum = T5CompanySatuan.Maksimum
-                 });
-                output.Barang.AddRange(qryBarang);
-            }
-            return Task.FromResult(output);
         }
 
         public override async Task GetDataBarangWithStream(ListBarangRequest request, IServerStreamWriter<Barang> responseStream, ServerCallContext context)
@@ -96,7 +37,7 @@ namespace grpcArachne
                  join T5CompanySatuan in _db.T5CompanySatuanDbSet
                  on T3Satuan.IdSatuan equals T5CompanySatuan.IdSatuan
                  join T1SubDivisiBarang in _db.T1SubDivisiBarangDbSet
-                 on T2Barang.IdSubDivisiBarang  equals T1SubDivisiBarang.IdSubDivisiBarang
+                 on T2Barang.IdSubDivisiBarang equals T1SubDivisiBarang.IdSubDivisiBarang
                  join T1SubKategoriBarang in _db.T1SubKategoriBarang
                  on T2Barang.IdSubKategoriBarang equals T1SubKategoriBarang.IdSubKategoriBarang
                  select new Barang
@@ -105,7 +46,7 @@ namespace grpcArachne
                      IdDivisiBarang = (long)T1SubDivisiBarang.IdDivisiBarang,
                      IdSubDivisiBarang = (long)T1SubDivisiBarang.IdSubDivisiBarang,
                      IdKategoriBarang = (long)T1SubKategoriBarang.IdKategoriBarang,
-                     IdSubKategoriBarang = (long)T1SubKategoriBarang.IdSubKategoriBarang,
+                     IdSubKategoriBarang = (long)T1SubKategoriBarang.IdKategoriBarang,
                      IdSupplier = (long)T1Supplier.IdSupplier,
                      IdCompany = T5CompanySatuan.IdCompany,
                      IdJenisSupplier = (long)T1Supplier.IdJenisSupplier,
@@ -126,7 +67,7 @@ namespace grpcArachne
                 var qryBarang =
                 (from T2Barang in _db.T2BarangDbSet
                  join T3Satuan in _db.T3SatuanDbSet
-                 on T2Barang.IdBarang equals T3Satuan.IdSatuan
+                 on T2Barang.IdBarang equals T3Satuan.IdBarang
                  join T5SupplierSatuan in _db.T5SupplierSatuanDbSet
                  on T3Satuan.IdSatuan equals T5SupplierSatuan.IdSatuan
                  join T1Supplier in _db.T1SupplierDbSet
@@ -146,7 +87,7 @@ namespace grpcArachne
                      IdDivisiBarang = (long)T1SubDivisiBarang.IdDivisiBarang,
                      IdSubDivisiBarang = (long)T1SubDivisiBarang.IdSubDivisiBarang,
                      IdKategoriBarang = (long)T1SubKategoriBarang.IdKategoriBarang,
-                     IdSubKategoriBarang = (long)T1SubKategoriBarang.IdSubKategoriBarang,
+                     IdSubKategoriBarang = (long)T1SubKategoriBarang.IdKategoriBarang,
                      IdSupplier = (long)T1Supplier.IdSupplier,
                      IdCompany = T5CompanySatuan.IdCompany,
                      IdJenisSupplier = (long)T1Supplier.IdJenisSupplier,
