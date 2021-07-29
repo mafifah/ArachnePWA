987a2c365d5f9fddfb55068f636bf34881c7cf5c
Deva Trivanus <devadlsc18@gmail.com>
2021-07-29 16:51:16 +0700

    Commit Merge

--- bzrArachne/obj/Debug/net5.0/RazorDeclaration/Pages/FormPenawaran.razor.g.cs
+++ bzrArachne/obj/Debug/net5.0/RazorDeclaration/Pages/FormPenawaran.razor.g.cs
@@ -13,126 +13,126 @@ namespace bzrArachne.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "D:\Arachne\bzrArachne\_Imports.razor"
+#line 1 "D:\ArachnePWA\bzrArachne\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\Arachne\bzrArachne\_Imports.razor"
+#line 2 "D:\ArachnePWA\bzrArachne\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\Arachne\bzrArachne\_Imports.razor"
+#line 3 "D:\ArachnePWA\bzrArachne\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\Arachne\bzrArachne\_Imports.razor"
+#line 4 "D:\ArachnePWA\bzrArachne\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "D:\Arachne\bzrArachne\_Imports.razor"
+#line 5 "D:\ArachnePWA\bzrArachne\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "D:\Arachne\bzrArachne\_Imports.razor"
+#line 6 "D:\ArachnePWA\bzrArachne\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "D:\Arachne\bzrArachne\_Imports.razor"
+#line 7 "D:\ArachnePWA\bzrArachne\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "D:\Arachne\bzrArachne\_Imports.razor"
+#line 8 "D:\ArachnePWA\bzrArachne\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "D:\Arachne\bzrArachne\_Imports.razor"
+#line 9 "D:\ArachnePWA\bzrArachne\_Imports.razor"
 using bzrArachne;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "D:\Arachne\bzrArachne\_Imports.razor"
+#line 10 "D:\ArachnePWA\bzrArachne\_Imports.razor"
 using bzrArachne.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "D:\Arachne\bzrArachne\_Imports.razor"
+#line 11 "D:\ArachnePWA\bzrArachne\_Imports.razor"
 using Blazor.AdminLte;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "D:\Arachne\bzrArachne\_Imports.razor"
+#line 12 "D:\ArachnePWA\bzrArachne\_Imports.razor"
 using Blazored.Toast;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 13 "D:\Arachne\bzrArachne\_Imports.razor"
+#line 13 "D:\ArachnePWA\bzrArachne\_Imports.razor"
 using Blazored.Toast.Services;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 14 "D:\Arachne\bzrArachne\_Imports.razor"
+#line 14 "D:\ArachnePWA\bzrArachne\_Imports.razor"
 using CurrieTechnologies.Razor.SweetAlert2;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 15 "D:\Arachne\bzrArachne\_Imports.razor"
+#line 15 "D:\ArachnePWA\bzrArachne\_Imports.razor"
 using MudBlazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\Arachne\bzrArachne\Pages\FormPenawaran.razor"
+#line 2 "D:\ArachnePWA\bzrArachne\Pages\FormPenawaran.razor"
 using bzrArachne.Service;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\Arachne\bzrArachne\Pages\FormPenawaran.razor"
+#line 3 "D:\ArachnePWA\bzrArachne\Pages\FormPenawaran.razor"
 using bzrArachne.Models;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\Arachne\bzrArachne\Pages\FormPenawaran.razor"
+#line 4 "D:\ArachnePWA\bzrArachne\Pages\FormPenawaran.razor"
 using BlazorNumericTextBox;
 
 #line default
@@ -147,7 +147,7 @@ using BlazorNumericTextBox;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 449 "D:\Arachne\bzrArachne\Pages\FormPenawaran.razor"
+#line 443 "D:\ArachnePWA\bzrArachne\Pages\FormPenawaran.razor"
  
     //MODAL
     void CheckboxClicked(DataBarang ItemC, object checkedValue)
