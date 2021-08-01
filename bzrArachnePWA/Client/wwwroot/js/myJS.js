0f5b6fa32752409fbd073de04dde12b465c71af1
Deva Trivanus <devadlsc18@gmail.com>
2021-08-01 16:38:14 +0700

    Buat Faktur

---
+++ bzrArachnePWA/Client/wwwroot/js/myJS.js
@@ -0,0 +1,16 @@
+﻿function myFaktur() {
+    var divContents = document.getElementById("myFaktur").innerHTML;
+    var a = window.open('', '_blank', 'height=500, width=500');
+    a.document.write('<html>');
+    a.document.write('<style type = "text/css">');
+    var table_style = document.getElementById("table_style").innerHTML;
+    a.document.write(table_style);
+    a.document.write('</style>');
+    a.document.write('</head><body >');
+    a.document.write('<body > <br>');
+    a.document.write(divContents);
+    a.document.write('</body></html>');
+    a.document.close();
+    a.print();
+}
+
