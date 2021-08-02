b589b6f16c9b82bb885475620306afbf739f9ad5
Deva Trivanus <devadlsc18@gmail.com>
2021-08-02 18:32:06 +0700

    Print Not Open New Page

--- bzrArachnePWA/Client/wwwroot/js/myJS.js
+++ bzrArachnePWA/Client/wwwroot/js/myJS.js
@@ -1,18 +1,38 @@
-﻿function myFaktur() {
-    var divContents = document.getElementById("myFaktur").innerHTML;
-    var a = window.open('', '_blank', 'height=500, width=500');
-    a.document.write('<html>');
-    a.document.write('<style type = "text/css">');
-    var table_style = document.getElementById("table_style").innerHTML;
-    a.document.write(table_style);
-    a.document.write('</style>');
-    a.document.write('</head><body >');
-    a.document.write('<body > <br>');
-    a.document.write(divContents);
-    a.document.write('</body></html>');
-    a.document.close();
-    a.focus();
-    a.print();
-    setTimeout(a.close, 500);
+﻿////function myFaktur() {
+////    var divContents = document.getElementById("myFaktur").innerHTML;
+////    var a = window.open( 'height=500, width=500');
+////    //a.document.write('<html>');
+////    //a.document.write('<style type = "text/css">');
+////    //a.document.write('</style>');
+////    //a.document.write('</head><body >');
+////    //a.document.write('<body > <br>');
+////    //a.document.write(divContents);
+////    //a.document.write('</body></html>');
+////    a.document.close();
+////    a.focus();
+////    self.print();
+////}
+
+
+function myFaktur() {
+    var contents = document.getElementById("myFaktur").innerHTML;
+    var frame1 = document.createElement('iframe');
+    frame1.name = "frame1";
+    frame1.style.position = "absolute";
+    frame1.style.top = "-1000000px";
+    document.body.appendChild(frame1);
+    var frameDoc = frame1.contentWindow ? frame1.contentWindow : frame1.contentDocument.document ? frame1.contentDocument.document : frame1.contentDocument;
+    frameDoc.document.open();
+    frameDoc.document.write('<html><head><title>DIV Contents</title>');
+    frameDoc.document.write('</head><body>');
+    frameDoc.document.write(contents);
+    frameDoc.document.write('</body></html>');
+    frameDoc.document.close();
+    setTimeout(function () {
+        window.frames["frame1"].focus();
+        window.frames["frame1"].print();
+        document.body.removeChild(frame1);
+    }, 500);
+    return false;
 }
 
